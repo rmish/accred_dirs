5ae0ca52065f0a152c86d5a6a3974f83a852811f
Mikhail Riapolov <rmish@yandex.ru>
2021-02-07 17:42:31 +0300

    Исправление кодировки и ошибок в работе маршрутизатора уровня подготовки

--- create_dirs.ps1
+++ create_dirs.ps1
@@ -1,61 +1,82 @@
-Function createSPO ($data)
+﻿Function createSPO ($data)
 {
-    write-host ("СПО")
+    write-output ($data.level)
 }
 
 Function createSpecialist ($data)
 {
-    write-host ("Специалитет")
+    write-output ("Специалитет")
 }
 
-Function createAspirat ($data)
+Function createAspirant ($data)
 {
-    Write-host("Аспирантура")
+    Write-output("Аспирантура")
 }
 
 Function createOrdinator ($data)
 {
-    write-host("Ординатура")
+    write-output("Ординатура")
 }
 
 Function createBachelor ($data)
 {
-    write-host("Бакалавриат")
+    write-output("Бакалавриат")
 }
 
 Function createMagister ($data)
 {
-    write-host("Магистратура")
+    write-output("Магистратура")
 }
 
 Function logRecord($data)
 {
-    write-host($data)
+    write-output($data)
 }
 
 
-$CoursesData = Import-Csv -Path "./regdata/test_reestr.csv" -Delimiter ";"
+$CoursesData = Import-Csv -Path "./regdata/test_reestr.csv" -Delimiter ";" -Encoding utf8
 
-foreach ($record in $CourseData)
+foreach ($record in $CoursesData)
 {
     switch ($record.level)
-        "СПО" {}
-        "Специалитет" {}
-        "Аспирантура" {}
-        "Балакалвират" {}
-        "Магистратура" {}
+    {
+        'СПО' {
+            createSPO($record) 
+            break
+        }
+        'Специалитет' {
+            createSpecialist($record)
+            break
+        }
+        'Аспирантура' {
+            createAspirant($record)
+            break
+        }
+        'Ординатура' {
+            createOrdinator($record)
+            break
+        }
+        'Бакалавриат' {
+            createBachelor($record)
+            break
+        }
+        'Магистратура' {
+            createMagister($record)
+            break
+        }
         default { logRecord($record) }
+    }
 }
 
 #[void](New-Item -itemtype directory "DIR1")
 #[void](New-Item -itemtype directory "DIR1/DIR2")
-$identity = "sysadmin_lab"
+## $identity = "sysadmin_lab"
 # Создание записи в ACL для модификации содержимого каталога
- $newAcl2 = Get-Acl DIR1
- $fsAList = $identity, "Modify", "None", "NoPropagateInherit", "Allow"
- $fsa2 = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fsAList
- $newAcl2.SetAccessRule($fsa2)
- Set-Acl -Path "DIR1" -AclObject $newAcl2
+## $newAcl2 = Get-Acl DIR1
+## $fsAList = $identity, "Modify", "None", "NoPropagateInherit", "Allow"
+## $fsa2 = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fsAList
+## $newAcl2.SetAccessRule($fsa2)
+## Set-Acl -Path "DIR1" -AclObject $newAcl2
 # Создание записи в acl для доступ ко всему созданому в каталоге содержимому
 # (наследуемые права для всех объектов в папке)
 # $newAcl = Get-Acl DIR1
@@ -64,4 +85,4 @@ $identity = "sysadmin_lab"
 # $newAcl.SetAccessRule($fsa)
 # Set-Acl -Path "DIR1" -AclObject $newAcl
 
-[void](New-Item -force -itemtype directory "DIR1/DIR2")
+##[void](New-Item -force -itemtype directory "DIR1/DIR2")
